457c5f8d03dc5cbf42f183fe1eff1afa64a72ced
pej556 <dmswn6173@naver.com>
2024-08-09 15:20:49 +0900

    2024.08.09 수정

--- JS/script.js
+++ JS/script.js
@@ -13,14 +13,19 @@ const swiper1 = new Swiper('#main_view', {
     // Optional parameters
     wrapperClass:'slider', //wrapperClass를 slider로 바꿤
     slideClass:'view',
+    autoplay: { //자동슬라이드
+      delay: 5000, //전환되기전 보여주는 시간
+    },
+    speed:800, //슬라이드가 전환되는(움직이는) 시간
+    spaceBetween:10, //슬라이드 사이간격
 
-    
 
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+      el: '.pager', //버튼영역 대상
+      bulletActiveClass:'active', //활성화된 요소에 추가되는 클래스
+      clickable:true, //클릭시 슬라이드 전환
     },
-
   });
 
 
